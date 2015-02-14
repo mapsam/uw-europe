6a23e181b5a7b4fbfbc286d6a9c9168ce3f0343c
svmatthews <matthews.sam@gmail.com>
2015-02-14 11:35:32 -0500

    country colors

---
+++ switch-countries.js
@@ -0,0 +1,56 @@
+function countryLabelPlacement(name) {
+  var attr;
+  switch(name) {
+    case "Albania":
+    case "Aland":
+    case "Andorra":
+    case "Austria":
+    case "Belgium":
+    case "Bulgaria":
+    case "Bosnia and Herzegovina":
+    case "Belarus":
+    case "Switzerland":
+    case "Czech Republic":
+    case "Germany":
+    case "Denmark":
+    case "Spain":
+    case "Estonia":
+    case "Finland":
+    case "France":
+    case "Faroe Islands":
+    case "United Kingdom":
+    case "Guernsey":
+    case "Greece":
+    case "Croatia":
+    case "Hungary":
+    case "Isle of Man":
+    case "Ireland":
+    case "Iceland":
+    case "Italy":
+    case "Jersey":
+    case "Kosovo":
+    case "Liechtenstein":
+    case "Lithuania":
+    case "Luxembourg":
+    case "Latvia":
+    case "Monaco":
+    case "Moldova":
+    case "Macedonia":
+    case "Malta":
+    case "Montenegro":
+    case "Netherlands":
+    case "Norway":
+    case "Poland":
+    case "Portugal":
+    case "Romania":
+    case "Russia":
+    case "San Marino":
+    case "Republic of Serbia":
+    case "Slovakia":
+    case "Slovenia":
+    case "Sweden":
+    case "Ukraine":
+    default:
+  }
+  return attr;
+}
