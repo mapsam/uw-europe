48abb15c77c1869e5d88472d330394df89b1737b
svmatthews <matthews.sam@gmail.com>
2015-02-14 14:29:34 -0500

    label placement ftw

--- static/europe.js
+++ static/europe.js
@@ -38,7 +38,7 @@ function drawMap(error, countries, land, places) {
     .data(topojson.feature(countries, countries.objects.countries).features)  
     .enter().append("path")
     .attr("class", "country")
-    .style("fill", function(d){ return countryColor(d.properties.admin); })
+    .style("fill", function(d){ return countryProperties(d.properties.admin).color; })
     .attr("d", path);
 
   /*
@@ -47,12 +47,15 @@ function drawMap(error, countries, land, places) {
   var countryLabels = svg.selectAll(".country-label")
     .data(topojson.feature(countries, countries.objects.countries).features)
     .enter().append("text")
-    .attr("class", "country-label")
+    .attr("class", function(d){ return countryProperties(d.properties.admin).className; })
     .attr("text-anchor", "middle")
     .attr("transform", function(d) {
-      var labelPoint = [path.centroid(d)[0], path.centroid(d)[1]];  
-      return "translate(" + labelPoint + ")";
+      var labelPoint = [path.centroid(d)[0], path.centroid(d)[1]];
+      var rotate = countryProperties(d.properties.admin).rotate;
+      return "translate(" + labelPoint + ") rotate(" + rotate + ")";
     })
+    .attr("dy", function(d){ return countryProperties(d.properties.admin).y; })
+    .attr("dx", function(d){ return countryProperties(d.properties.admin).x; })
     .text(function(d) { return d.properties.admin; });
 
   /*
@@ -74,9 +77,9 @@ function drawMap(error, countries, land, places) {
   var placeLabels = svg.selectAll('.place-label')
     .data(places.features).enter().append('text')
     .attr('transform', function(d) { return 'translate(' + projection(d.geometry.coordinates) + ')'; })
-    .attr('dy', function(d){ return placeLabelPlacement(d.properties.name).y; }) 
-    .attr('dx', function(d){ return placeLabelPlacement(d.properties.name).x; })
-    .style('text-anchor', function(d){ return placeLabelPlacement(d.properties.name).a; })
+    .attr('dy', function(d){ return placeProperties(d.properties.name).y; }) 
+    .attr('dx', function(d){ return placeProperties(d.properties.name).x; })
+    .style('text-anchor', function(d){ return placeProperties(d.properties.name).a; })
     .text(function(d) { return d.properties.name; })
     .style("visibility", function(d){
       if (d.properties.adm0cap) return "visible";
@@ -88,165 +91,464 @@ function drawMap(error, countries, land, places) {
     });
 }
 
-var colorCount = 0;
 var colors = ['#A8B6C7', '#9A8F8A', '#BDBC9C', '#E5D5B3', '#DED6CC'];
-function countryColor(name) {
+function countryProperties(name) {
   var attr;
   switch(name) {
     case "Albania": 
-      attr = colors[4];
+      attr = {
+        color: colors[4],
+        className: "country-label small dark",
+        x: -9,
+        y: 7,
+        rotate: 0
+      };
       break;
     case "Aland": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Andorra": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label small dark",
+        x: 25,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Austria": 
-      attr = colors[2];
+      attr = {
+        color: colors[2],
+        className: "country-label small",
+        x: 8,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Belgium": 
-      attr = colors[1];
+      attr = {
+        color: colors[1],
+        className: "country-label small",
+        x: 0,
+        y: 7,
+        rotate: 0
+      };
       break;
     case "Bulgaria": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label small",
+        x: 0,
+        y: 4,
+        rotate: 0
+      };
       break;
-    case "Bosnia and Herzegovina": 
-      attr = colors[4];
+    case "Bosnia & Herz.": 
+      attr = {
+        color: colors[4],
+        className: "country-label small",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Belarus": 
-      attr = colors[4];
+      attr = {
+        color: colors[4],
+        className: "country-label",
+        x: 0,
+        y: 10,
+        rotate: 0
+      };
       break;
     case "Switzerland": 
-      attr = colors[3];
+      attr = {
+        color: colors[3],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
-    case "Czech Republic": 
-      attr = colors[1];
+    case "Czech Rep.": 
+      attr = {
+        color: colors[1],
+        className: "country-label small",
+        x: 0,
+        y: 5,
+        rotate: 0
+      };
       break;
     case "Germany": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Denmark": 
-      attr = colors[3];
+      attr = {
+        color: colors[3],
+        className: "country-label dark",
+        x: -5,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Spain": 
-      attr = colors[1];
+      attr = {
+        color: colors[1],
+        className: "country-label",
+        x: 0,
+        y: 10,
+        rotate: 0
+      };
       break;
     case "Estonia": 
-      attr = colors[3];
+      attr = {
+        color: colors[3],
+        className: "country-label small dark",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Finland": 
-      attr = colors[1];
+      attr = {
+        color: colors[1],
+        className: "country-label",
+        x: 6,
+        y: 34,
+        rotate: 0
+      };
       break;
     case "France": 
-      attr = colors[2];
+      attr = {
+        color: colors[2],
+        className: "country-label",
+        x: 30,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Faroe Islands": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
-    case "United Kingdom": 
-      attr = colors[3];
+    case "U.K.": 
+      attr = {
+        color: colors[3],
+        className: "country-label",
+        x: 7,
+        y: 30,
+        rotate: 0
+      };
       break;
     case "Guernsey": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Greece": 
-      attr = colors[2];
+      attr = {
+        color: colors[2],
+        className: "country-label small",
+        x: -21,
+        y: -1,
+        rotate: 0
+      };
       break;
     case "Croatia": 
-      attr = colors[3];
+      attr = {
+        color: colors[3],
+        className: "country-label small",
+        x: 8,
+        y: -5,
+        rotate: 0
+      };
       break;
     case "Hungary": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: -4,
+        y: 5,
+        rotate: 0
+      };
       break;
     case "Isle of Man": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Ireland": 
-      attr = colors[1];
+      attr = {
+        color: colors[1],
+        className: "country-label small",
+        x: -2,
+        y: 16,
+        rotate: 0
+      };
       break;
     case "Iceland": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 4,
+        y: 7,
+        rotate: 0
+      };
       break;
     case "Italy": 
-      attr = colors[4];
+      attr = {
+        color: colors[4],
+        className: "country-label",
+        x: 2,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Jersey": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Kosovo": 
-      attr = colors[3];
+      attr = {
+        color: colors[3],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Liechtenstein": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Lithuania": 
-      attr = colors[1];
+      attr = {
+        color: colors[1],
+        className: "country-label small",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
-    case "Luxembourg": 
-      attr = colors[0];
+    case "Lux.": 
+      attr = {
+        color: colors[0],
+        className: "country-label small",
+        x: 16,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Latvia": 
-      attr = colors[2];
+      attr = {
+        color: colors[2],
+        className: "country-label small",
+        x: 12,
+        y: 3,
+        rotate: 0
+      };
       break;
     case "Monaco": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label small dark",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Moldova": 
-      attr = colors[3];
+      attr = {
+        color: colors[3],
+        className: "country-label small",
+        x: 0,
+        y: -5,
+        rotate: 0
+      };
       break;
-    case "Macedonia": 
-      attr = colors[1];
+    case "Mac.": 
+      attr = {
+        color: colors[1],
+        className: "country-label small",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Malta": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Montenegro": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label small dark",
+        x: -20,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Netherlands": 
-      attr = colors[4];
+      attr = {
+        color: colors[4],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Norway": 
-      attr = colors[2];
+      attr = {
+        color: colors[2],
+        className: "country-label",
+        x: -35,
+        y: 101,
+        rotate: 0
+      };
       break;
     case "Poland": 
-      attr = colors[3];
+      attr = {
+        color: colors[3],
+        className: "country-label",
+        x: -2,
+        y: 10,
+        rotate: 0
+      };
       break;
     case "Portugal": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 10,
+        rotate: -70
+      };
       break;
     case "Romania": 
-      attr = colors[1];
+      attr = {
+        color: colors[1],
+        className: "country-label",
+        x: -2,
+        y: 2,
+        rotate: 0
+      };
       break;
     case "Russia": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: -100,
+        y: 300,
+        rotate: 0
+      };
       break;
     case "San Marino": 
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
-    case "Republic of Serbia": 
-      attr = colors[2];
+    case "Serbia": 
+      attr = {
+        color: colors[2],
+        className: "country-label small",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Slovakia": 
-      attr = colors[4];
+      attr = {
+        color: colors[4],
+        className: "country-label small",
+        x: 7,
+        y: -2,
+        rotate: 0
+      };
       break;
     case "Slovenia": 
-      attr = colors[1];
+      attr = {
+        color: colors[1],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     case "Sweden": 
-      attr = colors[4];
+      attr = {
+        color: colors[4],
+        className: "country-label",
+        x: 10,
+        y: -10,
+        rotate: -60
+      };
       break;
     case "Ukraine": 
-      attr = colors[2];
+      attr = {
+        color: colors[2],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
     default:
-      attr = colors[0];
+      attr = {
+        color: colors[0],
+        className: "country-label",
+        x: 0,
+        y: 0,
+        rotate: 0
+      };
       break;
   }
   return attr;
 }
-function placeLabelPlacement(name) {
+function placeProperties(name) {
   var placements = {
     tr: {x: 2,  y: -2, a: "start" },
     tl: {x: -3, y: -2, a: "end" },
@@ -269,6 +571,9 @@ function placeLabelPlacement(name) {
     case "Podgorica":
       attr = placements.bl;
       break;
+    case "Kbenhavn":
+      attr = placements.br;
+      break;
     case "Tallinn":
       attr = placements.tl;
       break;
@@ -283,6 +588,18 @@ function placeLabelPlacement(name) {
       break;
     case "Riga":
       attr = placements.tl;
+      break;
+    case "Sofia":
+      attr = placements.tl;
+      break;
+    case "Vilnius":
+      attr = placements.bl;
+      break;
+    case "Lisbon":
+      attr = placements.bl;
+      break;
+    case "Luxembourg":
+      attr = placements.br;
       break;
     default:
       attr = placements.tr;
